4a7442ee180562a279016031f425c245521b8466
karinaleite-prog <karina.leite@aluno.facmais.edu.br>
2026-08-01 18:18:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const hoje = new Date();
 
+const inicioAno = new Date(hoje.getFullYear(), 0, 1);
+
+const diferenca = hoje - inicioAno;
+
+const diaDoAno = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+
+const indice = diaDoAno % conteudos.length;
+
+const conteudo = conteudos[indice];
+
+
+document.getElementById("tituloDia").innerHTML = conteudo.titulo;
+
+document.getElementById("mensagemDia").innerHTML = conteudo.mensagem;
+
+document.getElementById("imagemDia").src = conteudo.imagem;
+
+document.getElementById("musicaDia").href = conteudo.musica;
