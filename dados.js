6c1c2a5ade3fad68f2ff9898a737ef25fdb5fbd9
karinaleite-prog <karina.leite@aluno.facmais.edu.br>
2026-08-02 17:36:27 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,21 +3,21 @@ const conteudos = [
     titulo: "TODO AMANHECER É UMA NOVA CHANCE",
     mensagem: "Não importa o que aconteceu ontem. A vida sempre oferece um novo amanhecer para quem decide continuar. Confie no seu caminho. O melhor ainda está por vir. ❤️",
     imagem: "dia1.jpg",
-    musica: "dia1.mp3",
+    musica: "Musica/dia1.mp3",
   },
 
   {
     titulo: "VOCÊ É MAIS FORTE DO QUE PENSA",
     mensagem: "Cada desafio vencido fortalece sua coragem. Continue caminhando, mesmo que seja um passo de cada vez. 💙",
     imagem: "dia2.jpg",
-    musica: "dia2.mp3",
+    musica: "Musica/dia2.mp3",
   },
 
   {
     titulo: "A FELICIDADE ESTÁ NAS PEQUENAS COISAS",
     mensagem: "Valorize os pequenos momentos. Um sorriso, um abraço e um instante de paz podem transformar o seu dia. 🌸",
     imagem: "dia3.jpg",
-    musica: "dia3.mp3"
+    musica: "Musica/dia3.mp3"
   }
 ];
 
